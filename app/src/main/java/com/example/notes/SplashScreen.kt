529fcdf4d943b31844487497c49de291452b550d
Mahmoud <mahmoudahmed.razek@gmail.com>
2020-06-25 23:56:08 +0200

    write code of SplashScreen

--- app/src/main/java/com/example/notes/SplashScreen.kt
+++ app/src/main/java/com/example/notes/SplashScreen.kt
@@ -1,12 +1,36 @@
 package com.example.notes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.PendingIntent.getActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        hideSystemUI()
+
+        val intent = Intent(this, MainActivity::class.java)
+        Handler().postDelayed({
+            startActivity(intent)
+            finish()
+        }, 4000)
+
+    }
+
+    private fun hideSystemUI() {
+        val decorView = window.decorView
+        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 }
